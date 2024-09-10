8376d869b897644d035f70144d73d22ee89caaa6
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-09-10 09:36:20 +0200

    feat: Add database backup functionality with ORM

--- backend/controllers/backup/add_backup.go
+++ backend/controllers/backup/add_backup.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	utils "backend/controllers/utils"
+	model "backend/model"
 	"backend/services"
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"time"
 
@@ -45,4 +49,91 @@ func AddBackup(c *gin.Context) {
 		c.JSON(500, gin.H{"Message": "Error creating backup directory", "Error": err.Error()})
 		return
 	}
+
+	err = performDatabaseBackup(filepath, *database)
+	if err != nil {
+		c.JSON(500, gin.H{"Message": "Error creating backup", "Error": err.Error()})
+		return
+	}
+
+	backup := model.Backup{
+		DatabaseID: database.ID,
+		Status:     "success",
+		BackupType: "manual",
+		Filename:   filename,
+		Size:       "0",
+		ErrorMsg:   "",
+		Log:        "",
+	}
+
+	backupService := services.NewBackupService()
+
+	backupResult, err := backupService.CreateBackup(backup.Filename, backup.Status, backup.BackupType, backup.Size, backup.ErrorMsg, backup.Log, backup.DatabaseID.String())
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(201, backupResult)
+}
+
+func performDatabaseBackup(filepath string, database model.Database) error {
+	var cmd *exec.Cmd
+
+	params := &utils.DBParams{
+		Host:     database.Host,
+		Port:     database.Port,
+		Username: database.Username,
+		Password: database.Password,
+		DBName:   database.DatabaseName,
+		DBType:   database.Type,
+		SSLMode:  "disable",
+	}
+
+	co, err := utils.ConnectionTester(params)
+	if err != nil {
+		fmt.Println("Erreur de connexion:", err)
+		return err
+	}
+
+	if !co {
+		return fmt.Errorf("connection to database failed")
+	}
+
+	if database.Type == "mysql" {
+		cmd = exec.Command("mysqldump",
+			"-u", database.Username,
+			"-h", database.Host,
+			database.DatabaseName)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("MYSQL_PWD=%s", database.Password))
+	} else if database.Type == "postgres" {
+		cmd = exec.Command("pg_dump",
+			"-U", database.Username,
+			"-h", database.Host,
+			"-p", database.Port,
+			database.DatabaseName)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("PGPASSWORD=%s", database.Password))
+	} else {
+		return fmt.Errorf("unsupported database type: %s", database.Type)
+	}
+
+	outfile, err := os.Create(filepath)
+	if err != nil {
+		return fmt.Errorf("error creating output file: %v", err)
+	}
+	defer outfile.Close()
+
+	cmd.Stdout = outfile
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error executing dump command: %v, stderr: %s", err, stderr.String())
+	}
+
+	fmt.Println("Backup created successfully at", filepath)
+	return nil
 }
---
+++ backend/controllers/utils/connectionTester.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DBParams struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+	DBType   string
+}
+
+func ConnectionTester(params *DBParams) (bool, error) {
+	var dsn string
+
+	if params != nil {
+		switch params.DBType {
+		case "postgres":
+			dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+				params.Host, params.Port, params.Username, params.Password, params.DBName, params.SSLMode)
+		case "mysql":
+			dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+				params.Username, params.Password, params.Host, params.Port, params.DBName)
+		default:
+			return false, fmt.Errorf("unsupported database type: %s", params.DBType)
+		}
+	} else {
+		return false, fmt.Errorf("either connStr or params must be provided")
+	}
+
+	db, err := sql.Open(params.DBType, dsn)
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
