ba2661e36d2dff11571b56858ea1f78dbb4454ea
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-09-17 07:12:48 +0200

    feat: manual backup button

--- frontend/src/app/components/database-list/database-list.component.ts
+++ frontend/src/app/components/database-list/database-list.component.ts
@@ -3,22 +3,55 @@ import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { DatabaseService } from '../../services/database.service';
 import { NotificationService } from '../../services/notification.service';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { BackupService } from '../../services/backup.service';
+
+interface Database {
+  id: string;
+  name: string;
+  type: string;
+  host: string;
+  port: string;
+  username: string;
+  password: string;
+  database_name: string;
+  is_cron_active: boolean;
+  cron_schedule: string;
+  created_at: string;
+  updated_at: string;
+}
 
 @Component({
   selector: 'app-database-list',
   standalone: true,
-  imports: [TableModule, CommonModule],
+  imports: [
+    TableModule,
+    CommonModule,
+    ButtonModule,
+    MessageModule,
+    ToastModule,
+  ],
   templateUrl: './database-list.component.html',
   styleUrls: ['./database-list.component.css'],
-  providers: [DatabaseService],
+  providers: [
+    DatabaseService,
+    MessageService,
+    NotificationService,
+    BackupService,
+  ],
 })
 export class DatabaseListComponent implements OnInit {
-  databases: any[] = [];
+  databases: Database[] = [];
   errorMessage: string = '';
 
   constructor(
     private databaseService: DatabaseService,
-    private notificationService: NotificationService
+    private messageService: MessageService,
+    private notificationService: NotificationService,
+    private backupService: BackupService
   ) {}
 
   ngOnInit(): void {
@@ -31,7 +64,7 @@ export class DatabaseListComponent implements OnInit {
 
   loadDatabases() {
     this.databaseService.getDatabases().subscribe({
-      next: (data) => {
+      next: (data: Database[]) => {
         this.databases = data;
       },
       error: (error) => {
@@ -40,5 +73,36 @@ export class DatabaseListComponent implements OnInit {
         console.error(error);
       },
     });
+  }
+
+  createBackup(databaseId: string) {
+    console.log('Creating backup for database with ID:', databaseId);
+    if (!databaseId) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erreur',
+        detail: 'Veuillez sélectionner une base de données.',
+      });
+      return;
+    }
+    this.backupService.createBackup(databaseId).subscribe({
+      next: (data) => {
+        console.log('Backup created', data);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Succès',
+          detail: 'Backup créé avec succès.',
+        });
+      },
+      error: (error) => {
+        console.error(error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erreur',
+          detail: 'Erreur lors de la création du backup.',
+        });
+      },
+    });
+    // Ajoutez ici la logique pour créer un backup en utilisant l'ID de la base de données
   }
 }
--- frontend/src/app/services/backup.service.ts
+++ frontend/src/app/services/backup.service.ts
@@ -2,12 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface Backup {
+  id: string;
+  database_id: string;
+  status: string;
+  backup_type: string;
+  filename: string;
+  size?: string;
+  error_msg?: string;
+  log?: string;
+  created_at: string;
+  updated_at: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class BackupService {
   constructor(private http: HttpClient) {}
-  getBackups(): Observable<any> {
+  getBackups(): Observable<Backup> {
     return this.http.get<any>(`http://localhost:8080/backups`);
+  }
+
+  createBackup(databaseId: string): Observable<Backup> {
+    return this.http.post<any>(`http://localhost:8080/backup`, {
+      database_id: databaseId,
+    });
   }
 }
