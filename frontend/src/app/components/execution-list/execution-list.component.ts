8f6d4ab93ad011034cdd47ccc5372d0696e39d20
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-09-20 21:49:28 +0200

    refactor: Update execution list component
    - Update the HTML template of the execution list component to display additional columns for actions, type, status, database name, and created at.
    - Add buttons for actions in each row of the table.
    - Display the execution type using a tag component with appropriate severity.
    - Import CommonModule, TableModule, and TagModule in the execution list component.
    - Modify the ngOnInit method to assign the items property of the executions response to the executions array.
    - Add a getExecutionType method to map the execution type to the corresponding tag severity.

--- frontend/src/app/components/execution-list/execution-list.component.ts
+++ frontend/src/app/components/execution-list/execution-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importer CommonModule
 import { ExecutionService } from '../../services/execution.service';
 import { MessageService } from 'primeng/api';
 import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-execution-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TableModule, TagModule],
   templateUrl: './execution-list.component.html',
   styleUrl: './execution-list.component.css',
-  providers: [ExecutionService, MessageService, TableModule],
+  providers: [ExecutionService, MessageService],
 })
 export class ExecutionListComponent implements OnInit {
   executions: any[] = [];
@@ -18,11 +20,21 @@ export class ExecutionListComponent implements OnInit {
   ngOnInit(): void {
     this.executionService.getExecutions().subscribe({
       next: (executions) => {
-        this.executions = executions;
+        this.executions = executions.items;
       },
       error: (error) => {
         console.error(error);
       },
     });
+  }
+  getExecutionType(type: string) {
+    switch (type) {
+      case 'restore':
+        return 'warning';
+      case 'backup':
+        return 'success';
+      default:
+        return 'danger';
+    }
   }
 }
